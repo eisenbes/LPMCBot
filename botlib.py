d98b4023fa4b6d48cdd9d28fced31e110427c5bf
SlimTim10 <slimtim10@gmail.com>
2012-06-28 12:29:05 -0400

    Fixed indentation in all files.

--- botlib.py
+++ botlib.py
@@ -1,35 +1,35 @@
 import random
-
+    
 # To-do: allow for multiple admins (make all the appropriate changes in code)
-ADMIN = "SlimTim10"			# Admin name(s) for certain commands
-
+ADMIN = "SlimTim10"             # Admin name(s) for certain commands
+    
 def parsemsg(privmsg):
 # Split the received PRIVMSG message into two useful parts
 # Example message:
-# 	:SlimTim10!~SlimTim10@127-0-0-1.network.com PRIVMSG #channel :Hello?
-	parts = privmsg[1:].split(':', 1)
+#   :SlimTim10!~SlimTim10@127-0-0-1.network.com PRIVMSG #channel :Hello?
+    parts = privmsg[1:].split(':', 1)
 # The information part of the message (sender, "PRIVMSG", channel/nickname)
-	info = parts[0].split(' ')
-	msg = parts[1].rstrip()	# The message part (e.g., "Hello?")
+    info = parts[0].split(' ')
+    msg = parts[1].rstrip()	# The message part (e.g., "Hello?")
 # The sender of the message (e.g., "SlimTim10")
-	sender = info[0].split('!')[0]
+    sender = info[0].split('!')[0]
 # The string to be returned
-	ret = ''
+    ret = ''
 
 # Treat messages starting with '!' as commands (e.g., "!say hi")
-	if msg[0] == '!':
+    if msg[0] == '!':
 # Split command message into two parts: bot command and following text
-		cmd = msg.split(' ', 1)
+        cmd = msg.split(' ', 1)
 
 # The '!say' command makes the bot say something
 # To-do: make this command work for private messages sent directly to the bot
-		if cmd[0] == '!say':
+        if cmd[0] == '!say':
 # Send the message to where the '!say' command was sent
-			ret = 'PRIVMSG ' + info[2] + ' :' + cmd[1] + '\n'
+            ret = 'PRIVMSG ' + info[2] + ' :' + cmd[1] + '\n'
 
 # The '!die' command makes the bot quit (admin command)
-		if cmd[0] == '!die' and sender == ADMIN:
-			ret = 'QUIT\n'
+        if cmd[0] == '!die' and sender == ADMIN:
+            ret = 'QUIT\n'
 
 # To-do: add a '!calc' command that evaluates basic mathematical expressions
 # Difficulty: easy
@@ -40,33 +40,33 @@ def parsemsg(privmsg):
 
 # To-do: find and fix the bug
 # To-do: add helpful comments to this command's code
-		if cmd[0] == '!rps':
-			try:
-				user_rps = int(cmd[1])
-				if user_rps < 0 or user_rps > 3:
-					raise Exception("Invalid")
-				else:
-					rps_names = ['rock', 'paper', 'scissors']
-					bot_rps = random.randint(0, 3)
-					ret = 'PRIVMSG ' + info[2] + ' :Player chose ' + \
-					rps_names[user_rps] + '. LPMCBot chose ' + \
-					rps_names[bot_rps] + '. '
-					if user_rps == bot_rps:
-						ret += 'Tie game.\n'
-					else:
-						result = user_rps - bot_rps
-						if result == -2 or result == 1:
-							ret += 'Player wins!\n'
-						else:
-							ret += 'Player loses.\n'
-			except:
-				ret = 'PRIVMSG ' + info[2] + \
-				' :Command help: 0 = Rock, 1 = Paper, 3 = Scissors. ' + \
-				'Example: !rps 1\n'
+        if cmd[0] == '!rps':
+            try:
+                user_rps = int(cmd[1])
+                if user_rps < 0 or user_rps > 3:
+                    raise Exception("Invalid")
+                else:
+                    rps_names = ['rock', 'paper', 'scissors']
+                    bot_rps = random.randint(0, 3)
+                    ret = 'PRIVMSG ' + info[2] + ' :Player chose ' + \
+                    rps_names[user_rps] + '. LPMCBot chose ' + \
+                    rps_names[bot_rps] + '. '
+                    if user_rps == bot_rps:
+                        ret += 'Tie game.\n'
+                    else:
+                        result = user_rps - bot_rps
+                        if result == -2 or result == 1:
+                            ret += 'Player wins!\n'
+                        else:
+                            ret += 'Player loses.\n'
+            except:
+                ret = 'PRIVMSG ' + info[2] + \
+                ' :Command help: 0 = Rock, 1 = Paper, 3 = Scissors. ' + \
+                'Example: !rps 1\n'
 
 
 # To-do: add a '!ttt' command that starts a game of Tic Tac Toe to be played
 # 	against the bot
 # Difficulty: hard
 
-	return ret
+    return ret			# Return the appropriate string
